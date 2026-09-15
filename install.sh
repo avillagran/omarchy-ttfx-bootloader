1f02ce97b02cc78c0fc3749ef3c5ed2bb3e23f79
avillagran <andres@villagranquiroz.cl>
2026-09-15 05:55:29 -0300

    security: pin force-enabled installer archive

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-archive=https://github.com/avillagran/omarchy-ttfx-bootloader/archive/fd8af974e762279fb544cc495bdddd9df1363861.tar.gz
-expected_sha256=b02c2e7f2a11e761156b2393c46c068e0fb601af0c2036f70b3cb6d5f0fd54a1
+archive=https://github.com/avillagran/omarchy-ttfx-bootloader/archive/56005710184c2de905c6fb309dd8689bfca512de.tar.gz
+expected_sha256=a9b2bc89c7a6eedcfe0810b4ba27f1f5a0b2b2d4557bd8e6769b679db90021b9
 tmp=$(mktemp -d /tmp/omarchy-ttfx-bootloader.XXXXXXXX)
 trap 'rm -rf "$tmp"' EXIT
 
