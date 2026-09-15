53f82f5009f79ade76b7cfe6efe74a608b3872ad
avillagran <andres@villagranquiroz.cl>
2026-09-15 06:02:49 -0300

    security: pin force-enable correction

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-archive=https://github.com/avillagran/omarchy-ttfx-bootloader/archive/56005710184c2de905c6fb309dd8689bfca512de.tar.gz
-expected_sha256=a9b2bc89c7a6eedcfe0810b4ba27f1f5a0b2b2d4557bd8e6769b679db90021b9
+archive=https://github.com/avillagran/omarchy-ttfx-bootloader/archive/f17d59642b3dec06eca8edc34c7ea7c72887bb9f.tar.gz
+expected_sha256=dab670a6c7bf509fe06d6b75be123e558a0f46d9b2b36339b861fb39d2f47407
 tmp=$(mktemp -d /tmp/omarchy-ttfx-bootloader.XXXXXXXX)
 trap 'rm -rf "$tmp"' EXIT
 
