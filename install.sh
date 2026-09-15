ebffbf2194da4489cfd79357161912b7e3aa3109
avillagran <andres@villagranquiroz.cl>
2026-09-15 04:43:04 -0300

    security: update pinned bootstrap archive

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-archive=https://github.com/avillagran/omarchy-ttfx-bootloader/archive/060973a28d29adcbb2a47156bbed707ebfde59e4.tar.gz
-expected_sha256=c99fa3737d5786ce97c0534d34c20c2edac1134bec9162486a0099c03815e8ff
+archive=https://github.com/avillagran/omarchy-ttfx-bootloader/archive/679ff5bca5e7833b735cbd55022fc4417f7de9af.tar.gz
+expected_sha256=ec0e3462c920965fac2c92704b28bc3c706a565453d468f8699e89d75670b1f6
 tmp=$(mktemp -d /tmp/omarchy-ttfx-bootloader.XXXXXXXX)
 trap 'rm -rf "$tmp"' EXIT
 
