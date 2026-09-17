d70ae798aefd4950f1f3c594a461a129f88c328b
avillagran <andres@villagranquiroz.cl>
2026-09-17 20:46:57 -0300

    chore: pin one-line installer archive to the catalog fix

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-archive=${OMARCHY_TTFX_ARCHIVE:-https://github.com/avillagran/omarchy-ttfx-bootloader/archive/2eb89f55a4ea3db1b5629a85f466ed0a6b0755a6.tar.gz}
-expected_sha256=${OMARCHY_TTFX_SHA256:-c3ac653cc2612a34002c4c9bf78019afed5ab7ff25ee326d8db907cf106ad831}
+archive=${OMARCHY_TTFX_ARCHIVE:-https://github.com/avillagran/omarchy-ttfx-bootloader/archive/c4d3a8e5ae6039fb4f9acd3712d1e7a3b26e1e25.tar.gz}
+expected_sha256=${OMARCHY_TTFX_SHA256:-9b5c7f2668bbb27498a4931ba39b8da914854849085e0cc3b908a3de6bf07972}
 tmp=$(mktemp -d /tmp/omarchy-ttfx-bootloader.XXXXXXXX)
 trap 'rm -rf "$tmp"' EXIT
 
